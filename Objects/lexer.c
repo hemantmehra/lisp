7a8a52c0a93d61399328434f106b2f8b73896acd
Parveen Garg <parveen.garg@Samtelthales.com>
2017-02-18 17:58:29 +0530

    modified

--- Objects/lexer.c
+++ Objects/lexer.c
@@ -14,3 +14,46 @@ char* m_getstring(){
 	lexer_m.buffer[lexer_m.b_counter] = '\0';
 	return lexer_m.buffer;
 }
+int lp_func(char c) 
+ {if(c== '(') return 1; 
+ else return 0;}
+ 
+ int rp_func(char c) 
+ {if(c== ')') return 1; 
+ else return 0;}
+ 
+ 
+int dquote_func(char c)
+{if(c== '\"') 
+return 1; 
+ else return 0;}
+
+
+int digit_func(char c)
+{if(c>='0'&&c<='9') return 1; 
+ else return 0;}
+
+
+int plus_func(char c)
+{if(c== '+') return 1; 
+ else return 0;}
+
+int minus_func(char c)
+{if(c== '-') return 1; 
+ else return 0;}
+
+ int sym_char_func(char c)
+ {
+if(digit_func(c)||plus_func(c)||minus_func(c))
+return 1;
+else if(c>=65&&c<91||c>=97&&c<123)
+return 1;
+else if(c=='/'||c=='*'||c=='%')
+return 1;
+else
+return 0;
+}
+int whitespace_func(char c)
+{if(c == ' '||c=='\t'||c=='\n') return 1; 
+ else return 0;}
+ 
