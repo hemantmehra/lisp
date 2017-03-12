010385b38e1a35aa22564ef4baef8cabdaf6af98
hemantmehra <mehra.hemant14@gmail.com>
2017-03-12 16:31:33 +0530

    Add print_obj()

--- lisp.c
+++ lisp.c
@@ -5,14 +5,24 @@
 #include "interpret.h"
 #include "object.h"
 #include "intobject.h"
+#include "floatobject.h"
+#include "consobject.h"
+#include "symbolobject.h"
+#include "boolobject.h"
+#include "stringobject.h"
+#include "primprocobject.h"
+#include "closureobject.h"
+#include "nilobject.h"
 
+void print_obj(Object*);
 
 int main(){
 	printf("------------ Lisp Interpreter -------------\n");
 	printf("type quit to exit\n");
+	printf("-------------------------------------------\n");
 	init_env();
 	while(1){
-		printf("lisp>");
+		printf("lisp> ");
 		char str[100];
 		Object *obj;
 		fgets(str, 100, stdin);
@@ -22,10 +32,45 @@ int main(){
 
 		obj=interpret(str);
 
-		if(TYPE(obj) == INTEGER)
-			printf("%d\n", INT_VAL(obj));
-		//print_obj(obj);
+		print_obj(obj);
+		printf("\n");
 	}
 
 	return 0;
+}
+
+
+void print_obj(Object* obj){
+	switch(TYPE(obj)){
+		case INTEGER: 
+			printf("%d ", INT_VAL(obj));
+			break;
+		case FLOAT:
+			printf("%f ", FLOAT_VAL(obj));
+			break;
+		case CONS:
+			print_obj(CAR(obj));
+			print_obj(CDR(obj));
+			break;
+		case SYMBOL:
+			printf("%s ", SYMBOL_NAME(obj));
+			break;
+		case BOOLEAN:
+			if(BOOL_VAL(obj) == TRUE)
+				printf("TRUE ");
+			else
+				printf("FALSE ");
+			break;
+		case STRING:
+			printf("\"%s\" ", STRING(obj));
+			break;
+		case PRIM_PROC:
+			printf("<Primitive operation> ");
+			break;
+		case CLOSURE:
+			printf("<Closure> ");
+			break;
+		case NIL:
+			printf("NIL ");
+	}
 }
